c76ad5a1bcc19df69098792c12f9e45907638c7a
Mahima Manseta <98904805+Mahima-Manseta@users.noreply.github.com>
2022-08-31 12:25:21 +0530

    Add files via upload

---
+++ ComTaskManagementSprint1Application.java
@@ -0,0 +1,15 @@
+package com.taskmanagement.sprint1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComTaskManagementSprint1Application {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		SpringApplication.run(ComTaskManagementSprint1Application.class, args);
+		System.out.println("end!!!!!!");
+	}
+
+}
